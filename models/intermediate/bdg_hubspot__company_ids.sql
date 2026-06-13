414aba6662d2d287408888731ae760748d0f1085
Jack Galvin <jackgalvin99@gmail.com>
2026-06-13 16:17:11 +1000

    int models

---
+++ models/intermediate/bdg_hubspot__company_ids.sql
@@ -0,0 +1,39 @@
+with companies as (
+
+	select * from {{ ref('stg_hubspot__companies') }}
+
+),
+
+split_ids as (
+
+	select
+		company_id,
+		trim(unnest(regexp_split_to_array(coalesce(merged_object_ids, ''), '[;,]'))) as merged_object_id
+
+	from companies
+
+),
+
+final as (
+
+	select distinct
+		company_id,
+		case
+			when merged_object_id = '' then company_id
+			else merged_object_id
+		end as merged_object_id
+
+	from split_ids
+	where merged_object_id <> ''
+
+	union all
+
+	select
+		company_id,
+		company_id as merged_object_id
+
+	from companies
+
+)
+
+select * from final
